baa9b692e6d32b79be6196f1e429bc15546b9a9d
Amirhussein soori <amirhusseinsppro75@gmail.com>
2022-01-29 21:32:23 +0330

    create mvvm base

--- app/src/main/java/com/amirhusseinsoori/shared_resource/MainActivity.kt
+++ app/src/main/java/com/amirhusseinsoori/shared_resource/MainActivity.kt
@@ -17,19 +17,16 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {git
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val viewModel: SharedResourceViewModel = hiltViewModel()
 
             Shared_resourceTheme {
-
-
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                    val data= viewModel.stateFlow.collectAsState()
                     data.let {
-                        Log.e("TAG", "onCreate:  increment : ${ it.value.increment}   increment : ${ it.value.decrement}  ", )
+                        Log.e("Tag", "onCreate:  increment : ${ it.value.increment}   increment : ${ it.value.decrement}  ", )
 
                     }
                 }
@@ -38,15 +35,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Shared_resourceTheme {
-        Greeting("Android")
-    }
-}
